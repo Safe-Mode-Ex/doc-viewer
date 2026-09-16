6dbfa4a6892cc8bdbc9a435e32f4e6f847090fa3
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-09-16 21:25:59 +0300

    chore: set eslint

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    // Enable typed linting via the typescript-eslint Project Service.
+    // This is slower than untyped linting; see https://typescript-eslint.io/getting-started/typed-linting
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
