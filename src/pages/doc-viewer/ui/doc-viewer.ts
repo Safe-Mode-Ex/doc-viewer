486baaa1b0daf6ddabb600959b6e6085281e200f
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-09-21 15:38:10 +0300

    refactor: take away component page info addition

--- src/pages/doc-viewer/ui/doc-viewer.ts
+++ src/pages/doc-viewer/ui/doc-viewer.ts
@@ -7,6 +7,7 @@ import { DocViewerFacade } from '../model/doc-viewer-facade';
 import { Toolbar } from './toolbar/toolbar';
 import { AnnotationView } from './annotation/annotation-view';
 import { AnnotationEvent, AnnotationEventKind } from './annotation/annotation-type';
+import { DocumentPage } from '@shared/model';
 
 const ANNOTATIONS_CLASSNAME = 'page__annotations';
 const A4_PAGE_RATIO = '210 / 297';
@@ -29,12 +30,7 @@ export class DocViewer {
   protected readonly documentId = signal<string>(this.route.snapshot.paramMap.get('id') ?? '1');
   protected readonly pageRatio = signal<string>(A4_PAGE_RATIO);
   protected readonly pages = computed(() =>
-    this.facade.pages().map((page) => ({
-      ...page,
-      label: `Страница ${page.number.toString()}`,
-      imageAlt: `Изображение страницы ${page.number.toString()}`,
-      annotationsLabel: `Аннотации к странице ${page.number.toString()}`,
-    })),
+    this.facade.pages().map((page) => DocViewer.getPageAdditionalInfo(page)),
   );
 
   protected readonly documentLoader = resource({
@@ -106,5 +102,14 @@ export class DocViewer {
         this.facade.addTextAnnotation(pageNumber, text.trim());
       }
     }
+  }
+
+  protected static getPageAdditionalInfo(page: DocumentPage) {
+    return {
+      ...page,
+      label: `Страница ${page.number.toString()}`,
+      imageAlt: `Изображение страницы ${page.number.toString()}`,
+      annotationsLabel: `Аннотации к странице ${page.number.toString()}`,
+    };
   }
 }
